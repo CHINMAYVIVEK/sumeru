f26e6d78d8ccb5ad931ca40f7917ec03ea67fb10
AIRONAX Developer <aironax@outlook.com>
2026-08-13 23:50:42 +0530

    Move module unit tests to test/module and export sync test helpers.
    - Relocate reload, data_sync_load, and data_sync_views tests from core/module
    - Export ModuleViewDeleteQuery, AllowMenuParseFallback, RecordsFromActions, InferSysViewTypeFromArch

---
+++ test/module/data_sync_load_test.go
@@ -0,0 +1,31 @@
+package module_test
+
+import (
+	"fmt"
+	"testing"
+
+	"sumeru/core/module"
+)
+
+func TestAllowMenuParseFallback(t *testing.T) {
+	if !module.AllowMenuParseFallback(nil) {
+		t.Fatal("nil error should allow menu fallback")
+	}
+	rootErr := fmt.Errorf("views/x.xml: module XML root must be <sumeru>, got <odoo>")
+	if module.AllowMenuParseFallback(rootErr) {
+		t.Fatal("invalid module root should not allow menu fallback")
+	}
+	unmarshalErr := fmt.Errorf("XML syntax error")
+	if !module.AllowMenuParseFallback(unmarshalErr) {
+		t.Fatal("unmarshal errors should allow menu fallback")
+	}
+}
+
+func TestRecordsFromActionsEmpty(t *testing.T) {
+	if module.RecordsFromActions(nil) != nil {
+		t.Fatal("nil actions should return nil")
+	}
+	if len(module.RecordsFromActions(nil)) != 0 {
+		t.Fatal("empty actions should return nil slice")
+	}
+}
